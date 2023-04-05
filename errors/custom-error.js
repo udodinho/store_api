33ca0bd4f5079c35af865fd301464c65a21f4b16
udodinho <udodinho200@gmail.com>
2023-04-05 14:44:31 -0700

    Add custom error

---
+++ errors/custom-error.js
@@ -0,0 +1,12 @@
+class CustomAPIError extends Error {
+    constructor(statusCode, message) {
+        this.statusCode = statusCode
+        super(message)
+    }
+}
+
+const createCustomError = (statusCode, msg) => {
+    return new CustomAPIError(statusCode, msg)
+}
+
+module.exports = { createCustomError,CustomAPIError }
